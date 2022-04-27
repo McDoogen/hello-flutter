2881568fcc435574441cd364334d5e836a064fe0
Douglas Smith <mcdoogen@gmail.com>
2022-04-27 14:56:54 -0400

    separate http example

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,53 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(const MyApp2());
+
+class MyApp2 extends StatefulWidget {
+  const MyApp2({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp2> createState() => _MyApp2State();
+}
+
+class _MyApp2State extends State<MyApp2> {
+  late Future<Thing> futureThing;
+
+  @override
+  void initState() {
+    super.initState();
+    futureThing = fetchThing();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Fetchy Example',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+            appBar: AppBar(title: const Text('App Bar Title')),
+            body: Center(
+                child: FutureBuilder<Thing>(
+                    future: futureThing,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Text(snapshot.data!.text);
+                      } else if (snapshot.hasError) {
+                        return Text('${snapshot.error}');
+                      }
+                      return const CircularProgressIndicator();
+                    }))));
+  }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,6 +70,31 @@ class MyApp extends StatelessWidget {
                 Icon(Icons.star, color: Colors.orange),
                 Icon(Icons.star, color: Colors.blue),
               ]))),
+    );
+  }
+}
+
+Future<Thing> fetchThing() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+  if (response.statusCode == 200) {
+    return Thing.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load http data!');
+  }
+}
+
+class Thing {
+  final String text;
+
+  const Thing({
+    required this.text,
+  });
+
+  factory Thing.fromJson(Map<String, dynamic> json) {
+    return Thing(
+      text: json['title'],
     );
   }
 }
