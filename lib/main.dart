a011ce6dbe9de82766e661f7430ac0521f0e8d4b
Douglas Smith <mcdoogen@gmail.com>
2022-05-01 21:13:37 -0400

    another sql attempt

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,87 @@ import 'package:sqflite/sqflite.dart';
 
 void main() => runApp(const MyApp());
 
+class RecipeHandler {
+  Future<Database> intializeDB() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    return openDatabase(
+      join(await getDatabasesPath(), 'recipe_database.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE recipes(id INTEGER PRIMARY KEY, name TEXT, difficulty TEXT)',
+        );
+      },
+      version: 1,
+    );
+  }
+
+  Future<void> insertRecipe(Recipe recipe) async {
+    final db = await intializeDB();
+    await db.insert(
+      'recipes',
+      recipe.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<Recipe>> recipes() async {
+    final db = await intializeDB();
+    final List<Map<String, dynamic>> maps = await db.query('recipes');
+    return List.generate(maps.length, (i) {
+      return Recipe(
+        id: maps[i]['id'],
+        name: maps[i]['name'],
+        difficulty: maps[i]['difficulty'],
+      );
+    });
+  }
+
+  Future<void> updateRecipe(Recipe recipe) async {
+    final db = await intializeDB();
+    await db.update(
+      'recipes',
+      recipe.toMap(),
+      where: 'id = ?',
+      whereArgs: [recipe.id],
+    );
+  }
+
+  Future<void> deleteRecipe(int id) async {
+    final db = await intializeDB();
+    await db.delete(
+      'recipes',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
+
+class SqlWidgetThing extends StatefulWidget {
+  const SqlWidgetThing({Key? key}) : super(key: key);
+
+  @override
+  State<SqlWidgetThing> createState() => _SqlWidgetThingState();
+}
+
+class _SqlWidgetThingState extends State<SqlWidgetThing> {
+  late RecipeHandler handler;
+
+  @override
+  void initState() {
+    super.initState();
+    handler = RecipeHandler();
+    handler.intializeDB().whenComplete(() async {
+      await this.recipes();
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
 class FutureThing extends StatefulWidget {
   const FutureThing({Key? key}) : super(key: key);
 
@@ -66,7 +147,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightGreen,
       ),
       home: DefaultTabController(
           length: 3,
