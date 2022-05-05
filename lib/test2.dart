43274eac18ff772298ec9ddbdc8a5b7a69a7e5d5
Douglas Smith <mcdoogen@gmail.com>
2022-05-05 15:12:46 -0400

    Scaffold tests

--- lib/test2.dart
+++ lib/test2.dart
@@ -5,13 +5,42 @@ class Test2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle style =
+        TextButton.styleFrom(primary: Theme.of(context).colorScheme.onPrimary);
+
     return MaterialApp(
-        title: 'Test 2 App',
-        theme: ThemeData(
-          primarySwatch: Colors.cyan,
-        ),
-        home: const Center(
-          child: Text("Welcome to Test 2!"),
-        ));
+      title: 'Test 2 App',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: ScaffoldTest(),
+    );
+  }
+}
+
+class ScaffoldTest extends StatefulWidget {
+  const ScaffoldTest({Key? key}) : super(key: key);
+
+  @override
+  State<ScaffoldTest> createState() => _ScaffoldTestState();
+}
+
+class _ScaffoldTestState extends State<ScaffoldTest> {
+  int _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Application Title')),
+      body: Center(child: Text('You have pressed the button $_count times.')),
+      bottomNavigationBar: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          child: Container(height: 50.0)),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => setState(() => _count++),
+          tooltip: 'Increment Counter',
+          child: const Icon(Icons.add)),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
   }
 }
